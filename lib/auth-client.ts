15ebfe9cb72a58055e949aa6b5e2fc1de38138b9
ighmaz <ighmazcool@gmail.com>
2025-12-07 19:30:31 +0530

    setup betterauth

---
+++ lib/auth-client.ts
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+});
+
+// Export commonly used hooks and functions
+export const { signIn, signUp, signOut, useSession } = authClient;
