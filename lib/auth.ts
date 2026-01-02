b595551ea92759e2b86a3ae56c0198ac00b9b189
ighmaz <ighmazcool@gmail.com>
2026-01-02 13:56:03 +0530

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { nextCookies } from "better-auth/next-js";
 
 const appURL =
   process.env.BETTER_AUTH_URL ||
@@ -36,4 +37,5 @@ export const auth = betterAuth({
   experimental: {
     joins: true,
   },
+  plugins: [nextCookies()],
 });
